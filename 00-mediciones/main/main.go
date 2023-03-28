17bb7c868d899aa8b47810fb0071913b7cfc8b54
AgusLacomi <aguslacomi@gmail.com>
2023-03-28 11:20:31 -0300

    Punto 00 mediciones Terminado, con pdf de graficos incluidos

--- 00-mediciones/main/main.go
+++ 00-mediciones/main/main.go
@@ -3,31 +3,41 @@ package main
 import (
 	"busquedas"
 	"fmt"
+	"ordenamiento"
 	"sort"
 	"time"
 	"utiles"
 )
 
 func main() {
-	arreglo := utiles.GenerarArreglo(10, 100000)
-	buscado := -1
 
-	//fmt.Println(arreglo)
+	arr := []int{1000, 5000, 10000, 15000, 20000, 25000, 50000, 100000, 150000, 200000, 250000, 500000, 1000000}
 
-	inicio := time.Now()
-	// Busqueda Lineal
-	fmt.Println(busquedas.BusLineal(arreglo, buscado))
-	fmt.Println("Busqueda Lineal: ", time.Since(inicio))
+	for _, tamaño := range arr {
 
-	inicio = time.Now()
-	// Ordenar el arreglo para la busqueda binaria
-	sort.Ints(arreglo)
-	fmt.Println("Ordenamiento: ", time.Since(inicio))
-	//fmt.Println(arreglo)
+		arreglo := utiles.GenerarArreglo(10, tamaño)
+		buscado := -1
 
-	inicio = time.Now()
-	// Busqueda Binaria
-	fmt.Println(busquedas.BusquedaBinaria(arreglo, buscado))
-	fmt.Println("Busqueda Binaria: ", time.Since(inicio))
+		//fmt.Println(arreglo)
 
+		inicio := time.Now()
+		// Busqueda Lineal
+		fmt.Println(busquedas.BusLineal(arreglo, buscado))
+		fmt.Println("Busqueda Lineal: ", time.Since(inicio))
+
+		inicio = time.Now()
+		ordenamiento.Burbuja(arreglo)
+		fmt.Println(time.Since(inicio))
+
+		inicio = time.Now()
+		// Ordenar el arreglo para la busqueda binaria
+		sort.Ints(arreglo)
+		fmt.Println("Ordenamiento: ", time.Since(inicio))
+		//fmt.Println(arreglo)
+
+		inicio = time.Now()
+		// Busqueda Binaria
+		fmt.Println(busquedas.BusquedaBinaria(arreglo, buscado))
+		fmt.Println("Busqueda Binaria: ", time.Since(inicio))
+	}
 }
